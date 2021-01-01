8230481ad8fab2a6533027c02f57f478976efea0
Jill04 <69854931+Jill04@users.noreply.github.com>
2021-01-01 19:24:00 +0530

    Update PriceConsumerV3.sol

--- contracts/PriceConsumerV3.sol
+++ contracts/PriceConsumerV3.sol
@@ -18,8 +18,8 @@ contract PriceConsumerV3 {
      * Aggregator: ETH/USD
      * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
      */
-    constructor() public {
-        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+    constructor(address _priceFeedAddress) public {
+        priceFeed = AggregatorV3Interface(_priceFeedAddress);
     }
 
     /**
